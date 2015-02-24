b55a1fe9ecbadbb7350e3ab482f980a7a8d0f76b
Vicky Gong <vickyg@mit.edu>
2015-02-24 15:43:58 -0500

    Adding basic testing skeleton and travis files

---
+++ pyfactorie/tests/test.py
@@ -0,0 +1,15 @@
+from pyfactorie.pyfactorie import FactorieParser
+from os import environ
+from unittest import TestCase
+
+class Tester(TestCase):
+
+    def setUp(self):
+        self.factorie = FactorieParser()   
+
+    def test_true(self):
+        # should fail if no factorie server is running
+        # make sure node stuff works (add child)
+        # parse sentence should throw error is no server is running
+        # for now, just leaving no tests
+        self.assertTrue(True)
